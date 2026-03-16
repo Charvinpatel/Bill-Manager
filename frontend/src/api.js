63caf60ed90748aa79b26b8ffb3aee3da61180f6
Charvinpatel <144683778+Charvinpatel@users.noreply.github.com>
2026-03-16 15:39:52 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-// const API = axios.create({ baseURL: 'https://bill-manager-sannipatel.onrender.com/api' })
+// const API = axios.create({ baseURL: 'https://bill-manager-nazk.onrender.com/api' })
 
 
 const API = axios.create({ baseURL: '/api' })
